faad58d0b0dbfc460e1a90137ecaa4d209a533a1
zhuyao-opendeveloper <15982075224@163.com>
2026-07-26 14:21:37 +0800

    fix: compile errors round 3

--- app/src/main/java/com/lightmark/ui/screens/todo/AddEditTodoScreen.kt
+++ app/src/main/java/com/lightmark/ui/screens/todo/AddEditTodoScreen.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalLayoutApi
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FilterChipDefaults
