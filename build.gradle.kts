bf4d12eacabc84b0ef637e3a74f10d8103945bb6
zhuyao-opendeveloper <15982075224@163.com>
2026-07-26 13:03:30 +0800

    ?? build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// 轻刻 LightMark - 顶层构建配置
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+}
