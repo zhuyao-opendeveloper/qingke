58b4b6d681747f7d9ba29f8f2693a2f9fbeb0b5d
zhuyao-opendeveloper <15982075224@163.com>
2026-07-26 16:22:56 +0800

    fix: add fillMaxWidth import

--- app/src/main/java/com/lightmark/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/lightmark/ui/screens/auth/LoginScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
